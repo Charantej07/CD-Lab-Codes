b170386a07e59714af3298930371cb8e9359fed5
Charan Teja <96309627+Charantej07@users.noreply.github.com>
2023-12-31 14:18:45 +0530

    Removed all files except parsers

--- LALR using YACC/lex.yy.c
+++ LALR using YACC/lex.yy.c
@@ -282,21 +282,21 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
-static yyconst short int yy_accept[10] =
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
+static yyconst short int yy_accept[11] =
     {   0,
-        1,    1,    4,    2,    3,    1,    2,    1,    0
+        0,    0,    6,    4,    3,    2,    2,    1,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    4,    4,    1,    4,    1,    4,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,29 +321,33 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[4] =
+static yyconst int yy_meta[6] =
     {   0,
-        1,    2,    1
+        1,    1,    1,    1,    2
     } ;
 
-static yyconst short int yy_base[11] =
+static yyconst short int yy_base[12] =
     {   0,
-        0,    0,    6,    0,    7,    2,    0,    0,    7,    3
+        0,    0,    7,    8,    8,    8,    8,    0,    0,    8,
+        4
     } ;
 
-static yyconst short int yy_def[11] =
+static yyconst short int yy_def[12] =
     {   0,
-        9,    1,    9,   10,    9,   10,   10,    6,    0,    9
+       10,    1,   10,   10,   10,   10,   10,   11,   11,    0,
+       10
     } ;
 
-static yyconst short int yy_nxt[11] =
+static yyconst short int yy_nxt[14] =
     {   0,
-        4,    5,    6,    7,    8,    9,    3,    9,    9,    9
+        4,    5,    6,    7,    8,    9,   10,    3,   10,   10,
+       10,   10,   10
     } ;
 
-static yyconst short int yy_chk[11] =
+static yyconst short int yy_chk[14] =
     {   0,
-        1,    1,    1,   10,    6,    3,    9,    9,    9,    9
+        1,    1,    1,    1,    1,   11,    3,   10,   10,   10,
+       10,   10,   10
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -357,14 +361,10 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "practise.l"
+#line 1 "calc.l"
 #define INITIAL 0
-#line 2 "practise.l"
-    #include<stdio.h>
-    #include<stdlib.h>
-    int value;
-    int sum=0;
-
+#line 2 "calc.l"
+#include "calc.tab.h"
 #line 369 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
@@ -516,7 +516,7 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 10 "practise.l"
+#line 5 "calc.l"
 
 #line 522 "lex.yy.c"
 
@@ -569,13 +569,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 11 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 8 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -601,35 +601,34 @@ do_action:	/* This label is used only to access EOF actions. */
 			yy_current_state = yy_last_accepting_state;
 			goto yy_find_action;
 
+
 case 1:
 YY_RULE_SETUP
-#line 11 "practise.l"
-{
-    if(yyleng!=4){
-        printf("Not accepted");
-    }else {
-        for(int i=0;i<4;i++){
-            sum+=(int)yytext[i]-48;
-        }
-        if(sum==9){
-            printf("String is accepted");
-        }else {
-            printf("String is not accepted");
-        }
-    }
-}
+#line 7 "calc.l"
+{ yylval.num=atoi(yytext); return NUM; }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 25 "practise.l"
-{printf("String is not accepted");}
+#line 8 "calc.l"
+{ return *yytext; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 26 "practise.l"
+#line 9 "calc.l"
+;
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 10 "calc.l"
+{ fprintf(stderr, "Invalid character: %s\n", yytext); }
+	YY_BREAK
+
+case 5:
+YY_RULE_SETUP
+#line 14 "calc.l"
 ECHO;
 	YY_BREAK
-#line 633 "lex.yy.c"
+#line 632 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -921,7 +920,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 11 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -956,11 +955,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 11 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 10);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1515,10 +1514,10 @@ int main()
 	return 0;
 	}
 #endif
-#line 26 "practise.l"
+#line 14 "calc.l"
 
-int yywrap(){}
-int main(){
-    printf("Enter the string: ");
-    yylex();
+
+int yywrap()
+{
+return 1;
 }
